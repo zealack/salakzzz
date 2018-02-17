c310df064e5f4c161cb92d8b672b36f8860e6c15
zealack <35425057+zealack@users.noreply.github.com>
2018-02-17 13:44:45 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/02/4-pertanyaan-paling-penting-sebelum.html"}
 if(x=="2"){xxx="/2018/02/apakah-anda-berbohong-tentang-tujuan.html"}
